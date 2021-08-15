fa95a528fda7547c149c81e40141ef97add10a63
Georgi <zkscpqm@daum.net>
2021-08-15 16:57:11 +0300

    Created Singleton and Config metaclasses

---
+++ meta/config_meta.py
@@ -0,0 +1,35 @@
+from typing import Any
+import os
+
+from types_extensions import void
+
+try:
+    import boto3
+    boto3_exists: bool = True
+except ImportError:
+    boto3_exists = False
+
+
+class BasicConfigMeta(type):
+
+    def __getattr__(self, key: str) -> Any:
+
+        for key_instance in (key, key.casefold(), key.upper(), key.capitalize()):
+            value = os.environ.get(key_instance)
+            if not value:
+                value = locals().get(key_instance)
+            if not value:
+                if boto3_exists:
+                    sm = boto3.client("secretsmanager")
+                    try:
+                        value = sm.get_secret_value(SecretId=key_instance).get("SecretString")
+                    except sm.exceptions.ResourceNotFoundException:
+                        ...
+            if value:
+                return value
+
+
+class FinalConfigMeta(BasicConfigMeta):
+
+    def __setattr__(self, key: str, value: Any) -> void:
+        raise AttributeError("You cannot update the configuration")
---
+++ meta/singleton_meta.py
@@ -0,0 +1,21 @@
+
+class SingletonMeta(type):
+
+    _instances: dict[str, type] = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls.__name__ not in cls._instances:
+            cls._instances[cls.__name__] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls.__name__]
+
+
+class ThreadLocalSingletonMeta(type):
+
+    _locals_key: str = "__singleton_instances"
+
+    def __call__(cls, *args, **kwargs):
+        if not locals().get(cls._locals_key):
+            locals()[cls._locals_key] = {}
+        if cls.__name__ not in locals()[cls._locals_key]:
+            locals()[cls._locals_key][cls.__name__] = super(ThreadLocalSingletonMeta, cls).__call__(*args, **kwargs)
+        return locals()[cls._locals_key][cls.__name__]
