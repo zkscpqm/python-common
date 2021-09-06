e38e03a92dc6484c2c760b68e0c52b731d6354c1
Georgi <zkscpqm@daum.net>
2021-09-06 05:09:26 +0300

    Fixed context table metaclass mro and created object serializer class

--- types_extensions.py
+++ types_extensions.py
@@ -5,6 +5,8 @@ _assert_py_version(PythonVersion(3, 6))
 from decimal import Decimal
 from typing import *
 
+void = NULL = null = nil = type(None)
+
 if get_current_version() >= PythonVersion(3, 9):
     list_type = list
     dict_type = dict
@@ -19,12 +21,15 @@ else:
 if get_current_version() >= PythonVersion(3, 10):
     Number_t = int | float | complex | Decimal
     string_like = str | bytes
+    primitives = int | float | void | string_like | bool
+    single_value_iterables = list_type | tuple_type | set_type | frozenset
 else:
     Number_t = Union[int, float, complex, Decimal]
     string_like = Union[str, bytes]
+    primitives = Union[int, float, void, string_like, bool]
+    single_value_iterables = Union[list_type, tuple_type, set_type, frozenset]
 
 Function = Method = Callable
-void = NULL = null = nil = type(None)
 HashMap = dict
 
 
@@ -34,3 +39,9 @@ def const(t_: type) -> type:
 
 def safe_type(type_: type) -> Union:
     return Union[type_, void]
+
+
+def is_of_type(actual_t: Any, expected_t: type) -> bool:
+    if hasattr(expected_t, '__args__'):
+        return actual_t in expected_t.__args__
+    return actual_t == expected_t
